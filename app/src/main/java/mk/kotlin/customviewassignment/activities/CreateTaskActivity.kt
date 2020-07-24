fb743e1caebe54645f0227333cd7d50e1b3a7a65
Mg Kaung <mymemory692018@gmail.com>
2020-07-24 22:37:22 +0630

    complete create task page

--- app/src/main/java/mk/kotlin/customviewassignment/activities/CreateTaskActivity.kt
+++ app/src/main/java/mk/kotlin/customviewassignment/activities/CreateTaskActivity.kt
@@ -3,7 +3,10 @@ package mk.kotlin.customviewassignment.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.create_task_toolbar.*
+import kotlinx.android.synthetic.main.create_task_vard_layout.*
 import mk.kotlin.customviewassignment.R
 
 class CreateTaskActivity : BaseActivity() {
@@ -17,7 +20,14 @@ class CreateTaskActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_task_layout)
-        backtext.setOnClickListener{onBackPressed()}
+        loadImage()
+        backtext.setOnClickListener { onBackPressed() }
+    }
+
+    private fun loadImage() {
+        Glide.with(profile)
+            .load("https://images.assetsdelivery.com/compings_v2/gmast3r/gmast3r1511/gmast3r151100776.jpg")
+            .apply(RequestOptions().circleCrop()).into(profile)
     }
 
     override fun onBackPressed() {
